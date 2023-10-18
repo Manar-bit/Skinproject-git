2a083e0421801160813c80ca0a14305befb51a34
DESKTOP-K6A93F5\chino <manar.benmaaouia.2000@gmail.com>
2023-10-18 12:16:35 +0200

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,284 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Acceuil -Le spécialiste de l'épilation</title>
+    <meta name="author" content="Angfuzsoft">
+    <meta name="description" content="Rasm - Nail Salon & Spa Beauty Care">
+    <meta name="keywords" content="Rasm - Nail Salon & Spa Beauty Care">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/img/favicons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="assets/img/favicons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600;700&amp;family=DM+Sans:wght@400;500;700&amp;display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <link rel="stylesheet" href="assets/css/odometer-theme-default.css">
+    <link rel="stylesheet" href="assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    @yield('styles')
+</head>
+
+<body>
+    <div class="preloader"><button class="th-btn style3 preloaderCls">Cancel Preloader</button>
+        <div class="preloader-inner"><img src="assets/img/logo.svg" alt="Rasm"> <span class="loader"></span></div>
+    </div>
+    <div class="th-menu-wrapper">
+        <div class="th-menu-area">
+            <div class="mobile-logo"><a href="index.html"><img src="assets/img/logo.svg" alt="Rasm"></a>
+                <div class="close-menu"><button class="th-menu-toggle"><i class="fal fa-times"></i></button></div>
+            </div>
+            <div class="th-mobile-menu">
+                <ul>
+                    <li class="menu-item-has-children"><a href="index.html">ACCUEIL11</a>
+                        <ul class="sub-menu">
+                            <li><a href="index.html">Spa Message Parlor</a></li>
+                            <li><a href="home-nail-stylist.html">Nail Stylist</a></li>
+                            <li><a href="home-hair-salon.html">Hair Salon</a></li>
+                            <li><a href="home-beauty-salon.html">Beauty Salon</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children"><a href="#">EPILATION LASER</a>
+                        <ul class="sub-menu">
+                            <li><a href="about.html">About One Spa & Nail</a></li>
+                            <li><a href="about-2.html">About Two Salon & Wellness</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children"><a href="#">TECHNOLOGIES</a>
+                        <ul class="sub-menu">
+                            <li><a href="service.html">Services One Spa & Nail</a></li>
+                            <li><a href="service-2.html">Services Two Salon & Wellness</a></li>
+                            <li><a href="service-details.html">Services Details</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children mega-menu-wrap"><a href="#">TARIFS</a>
+                        <ul class="mega-menu">
+                            <li><a href="shop.html">Pagelist 1</a>
+                                <ul>
+                                    <li><a href="index.html">Spa Message Parlor</a></li>
+                                    <li><a href="home-nail-stylist.html">Nail Stylist</a></li>
+                                    <li><a href="home-hair-salon.html">Hair Salon</a></li>
+                                    <li><a href="home-beauty-salon-2.html">Beauty Salon</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Pagelist 2</a>
+                                <ul>
+                                    <li><a href="about.html">About One Spa & Nail</a></li>
+                                    <li><a href="about-2.html">About Two Salon & Wellness</a></li>
+                                    <li><a href="service.html">Services One Spa & Nail</a></li>
+                                    <li><a href="service-2.html">Services Two Salon & Wellness</a></li>
+                                    <li><a href="service-details.html">Services Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Pagelist 3</a>
+                                <ul>
+                                    <li><a href="appointment.html">Appointment</a></li>
+                                    <li><a href="team.html">Team</a></li>
+                                    <li><a href="team-details.html">Team Details</a></li>
+                                    <li><a href="blog.html">Blog</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Pagelist 4</a>
+                                <ul>
+                                    <li><a href="gallery.html">Photo Gallery</a></li>
+                                    <li><a href="price.html">Price Table</a></li>
+                                    <li><a href="faq.html">FAQ</a></li>
+                                    <li><a href="contact.html">Contact Us</a></li>
+                                    <li><a href="error.html">Error Page</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children"><a href="#">CHÈQUES CADEAUX</a>
+                        <ul class="sub-menu">
+                            <li><a href="shop.html">Shop</a></li>
+                            <li><a href="shop-details.html">Shop Details</a></li>
+                            <li><a href="cart.html">Cart Page</a></li>
+                            <li><a href="checkout.html">Checkout</a></li>
+                            <li><a href="wishlist.html">Wishlist</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children"><a href="#">GALERIE</a>
+                        <ul class="sub-menu">
+                            <li><a href="#">Blog Layout</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog-grid-2-columns.html">Blog Grid 02 Columns</a></li>
+                                    <li><a href="blog-grid-3-columns.html">Blog Grid 03 Columns</a></li>
+                                    <li><a href="blog-list.html">Blog List</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="blog.html">Blog Sidebar</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                                    <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                                    <li><a href="blog.html">Blog No Sidebar</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="blog-details.html">Blog Details</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="contact.html"> À PROPOS</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="sidemenu-wrapper shopping-cart d-none d-lg-block">
+        <div class="sidemenu-content"><button class="closeButton sideMenuCls"><i class="far fa-times"></i></button>
+            <div class="widget woocommerce widget_shopping_cart">
+                <h3 class="widget_title">Shopping cart</h3>
+                <div class="widget_shopping_cart_content">
+                    <ul class="woocommerce-mini-cart cart_list product_list_widget">
+                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
+                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
+                                    src="assets/img/product/product_thumb_1_1.jpg" alt="Cart Image">OPI Nail Polish</a>
+                            <span class="quantity">1 × <span class="woocommerce-Price-amount amount"><span
+                                        class="woocommerce-Price-currencySymbol">$</span>94</span>,</span></li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
+                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
+                                    src="assets/img/product/product_thumb_1_2.jpg" alt="Cart Image">Sunscreen Lotion</a>
+                            <span class="quantity">1 × <span class="woocommerce-Price-amount amount"><span
+                                        class="woocommerce-Price-currencySymbol">$</span>89</span></span></li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
+                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
+                                    src="assets/img/product/product_thumb_1_3.jpg" alt="Cart Image">Wholesale Liquid</a>
+                            <span class="quantity">1 × <span class="woocommerce-Price-amount amount"><span
+                                        class="woocommerce-Price-currencySymbol">$</span>75</span></span></li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
+                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
+                                    src="assets/img/product/product_thumb_1_4.jpg" alt="Cart Image">Hair Dryers</a>
+                            <span class="quantity">1 × <span class="woocommerce-Price-amount amount"><span
+                                        class="woocommerce-Price-currencySymbol">$</span>72</span></span></li>
+                        <li class="woocommerce-mini-cart-item mini_cart_item"><a href="#"
+                                class="remove remove_from_cart_button"><i class="far fa-times"></i></a> <a href="#"><img
+                                    src="assets/img/product/product_thumb_1_5.jpg" alt="Cart Image">Argan Hair oil</a>
+                            <span class="quantity">1 × <span class="woocommerce-Price-amount amount"><span
+                                        class="woocommerce-Price-currencySymbol">$</span>81</span></span></li>
+                    </ul>
+                    <p class="woocommerce-mini-cart__total total"><strong>Subtotal:</strong> <span
+                            class="woocommerce-Price-amount amount"><span
+                                class="woocommerce-Price-currencySymbol">$</span>43</span></p>
+                    <p class="woocommerce-mini-cart__buttons buttons"><a href="cart.html" class="th-btn wc-forward">View
+                            cart</a> <a href="checkout.html" class="th-btn checkout wc-forward">Checkout</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sidemenu-wrapper d-none d-lg-block">
+        <div class="sidemenu-content bg-black2"><button class="closeButton sideMenuCls"><i
+                    class="far fa-times"></i></button>
+            <div class="widget footer-widget">
+                <div class="th-widget-about">
+                    <div class="about-logo"><a href="index.html"><img src="assets/img/logo-white.svg" alt="Rasm"></a>
+                    </div>
+                    <p class="about-text">Centric applications productize front end portals visualize front end is
+                        results and value added</p>
+                    <div class="working-time"><span class="title">We Are Available:</span>
+                        <p class="desc">Mon-Sat: 08.00 am to 5.00 pm</p>
+                    </div>
+                    <div class="th-social footer-social"><a href="https://www.facebook.com/"><i
+                                class="fab fa-facebook-f"></i></a> <a href="https://www.twitter.com/"><i
+                                class="fab fa-twitter"></i></a> <a href="https://www.linkedin.com/"><i
+                                class="fab fa-linkedin-in"></i></a> <a href="https://www.behance.net/"><i
+                                class="fa-brands fa-behance"></i></a></div>
+                </div>
+            </div>
+            <div class="widget footer-widget">
+                <h3 class="widget_title">Recent Posts</h3>
+                <div class="recent-post-wrap">
+                    <div class="recent-post">
+                        <div class="media-img"><a href="blog-details.html"><img
+                                    src="assets/img/blog/recent-post-2-1.jpg" alt="Blog Image"></a></div>
+                        <div class="media-body">
+                            <h4 class="post-title"><a class="text-inherit" href="blog-details.html">Local residents and
+                                    a few celebrities, too</a></h4>
+                            <div class="recent-post-meta"><a href="blog.html"><i class="fal fa-calendar-days"></i>19
+                                    June, 2023</a></div>
+                        </div>
+                    </div>
+                    <div class="recent-post">
+                        <div class="media-img"><a href="blog-details.html"><img
+                                    src="assets/img/blog/recent-post-2-2.jpg" alt="Blog Image"></a></div>
+                        <div class="media-body">
+                            <h4 class="post-title"><a class="text-inherit" href="blog-details.html">Local residents and
+                                    a few celebrities, too</a></h4>
+                            <div class="recent-post-meta"><a href="blog.html"><i class="fal fa-calendar-days"></i>22
+                                    June, 2023</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="widget footer-widget">
+                <h4 class="widget_title">Newsletter</h4>
+                <div class="newsletter-widget">
+                    <p class="md-10">Sign Up to get updates & news about us . Get Latest Deals from Walker's Inbox to
+                        our mail address.</p>
+                    <div class="footer-search-contact mt-25">
+                        <form><input class="form-control" type="email" placeholder="Enter your email"></form>
+                        <div class="footer-btn mt-10"><button type="submit" class="th-btn style3 fw-btn">Subscribe Now
+                                <i class="fa-regular fa-arrow-right"></i></button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="popup-search-box d-none d-lg-block"><button class="searchClose"><i class="fal fa-times"></i></button>
+        <form action="#"><input type="text" placeholder="What are you looking for?"> <button type="submit"><i
+                    class="fal fa-search"></i></button></form>
+    </div>
+ 
+
+    @include('layouts.header')
+
+    @yield('content')
+   
+    @include('layouts.footer')
+
+    <div class="scroll-top"><svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"
+                style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 307.919;">
+            </path>
+        </svg></div>
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery-ui.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/odometer.js"></script>
+    <script src="assets/js/appear-2.js"></script>
+    <script src="assets/js/nice-select.min.js"></script>
+    <script src="assets/js/jquery.datetimepicker.min.js"></script>
+    <script src="assets/js/tilt.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    @yield('scripts')
+</body>
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,65 @@
+<header class="th-header header-layout1">
+    <div class="header-top">
+        <div class="container th-container">
+            <div class="row justify-content-end justify-content-lg-between align-items-center gy-2">
+                <div class="col-auto d-none d-lg-block">
+                    <div class="header-links">
+                        <ul>
+                            <li><i class="far fa-phone"></i><a href="tel:+256214203215">+32 470 37 90 18</a></li>
+                            <li><i class="far fa-envelope"></i><a href="mailto:info@rasm.com">info@globalskin.be</a></li>
+                            <li><i class="fa-regular fa-location-dot"></i>Rue de la Pépinière 2A, 1000 Bruxelles</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-auto ">
+                    <div class="social-links"><span class="social-title">Rejoignez-nous sur</span> <a
+                            href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+
+                        <a href="https://www.linkedin.com/"><i class="fab fa-instagram"></i> </a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sticky-wrapper">
+        <div class="menu-area">
+            <div class="container th-container">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-auto">
+                        <div class="header-logo"><a href="index.html"><img src="assets/img/logo-globalskin.png" alt="Rasm"></a>
+                        </div>
+                    </div>
+                    <div class="col-auto me-xl-auto">
+                        <nav class="main-menu d-none d-lg-inline-block">
+                            <ul>
+                                <li ><a href="index.html">ACCEUIL</a>
+
+                                </li>
+                                <li ><a href="#">EPILATION LASER</a>
+
+                                </li>
+                                <li ><a href="#">TECHNOLOGIES</a>
+
+                                </li>
+                                <li ><a href="#">TARIFS</a>
+
+                                </li>
+                                <li ><a href="#">CHEQUES CADEAUX</a>
+                                </li>
+                                <li ><a href="#">GALERIE</a>
+                                </li>
+                                <li><a href="contact.html">A PROPOS </a></li>
+                                <li><a href="contact.html">Contact </a></li>
+                            </ul>
+                        </nav><button class="th-menu-toggle d-inline-block d-lg-none"><i
+                                class="far fa-bars"></i></button>
+                    </div>
+                    <div class="col-auto d-none d-xl-block">
+                        <a href="about.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="logo-bg"></div>
+        </div>
+    </div>
+</header>

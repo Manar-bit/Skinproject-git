cbef63f87d08550dcb498d6a84e6e1a034e03eda
manar <manar.benmaaouia.2000@gmail.com>
2023-10-24 15:41:31 +0100

    index tarifs contact

---
+++ resources/views/tarifs.blade.php
@@ -0,0 +1,224 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <section class="th-blog-wrapper space-top space-extra-bottom">
+        <div class="container th-container">
+            <div class="row">
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-1.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title" style="font-size: 32px;"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                           <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-2.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-3.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-4.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-5.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-6.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-7.png"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-8.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-9.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                    </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-10.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-11.png"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="th-blog blog-single has-post-thumbnail">
+                        <div class="blog-img"><a href="blog-details.html"><img src="assets/img/blog/blog-s-1-11.jpg"
+                                                                               alt="Blog Image"></a></div>
+                        <div class="blog-content">
+                            <div class="blog-meta">
+                                <p> A partir de</p>
+                                <span class="sub-title"  style="text-decoration: line-through;">350€</span>
+                                <span class="sub-title"  style="font-size: 25px ; color:#d09223 ">179€</span></div>
+                            <h2 class="blog-title"><a href="blog-details.html">Pack corps complet - Epilation définitive Bruxelles
+                                    Spas</a></h2>
+                            <a href="blog-details.html" class="th-btn" style="border-radius: 15px 0 15px ; box-shadow: 5px 5px 5px rgba(41,59,95,.25);">Réserver</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
